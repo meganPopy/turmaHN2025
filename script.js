7f69141c3c98ed4cbf9d06b88385476ea5ab4609
Tânia <106488757+meganPopy@users.noreply.github.com>
2025-03-18 19:12:53 -0300

    a

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-    })
+    
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
