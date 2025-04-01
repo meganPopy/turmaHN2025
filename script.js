3043dc9619a5fb13e6d28a0e9dd6fb21d7621d3f
Tânia <106488757+meganPopy@users.noreply.github.com>
2025-04-01 19:20:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     })
     
     let tamanhoAtualFonte = 1;
-
+}
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
